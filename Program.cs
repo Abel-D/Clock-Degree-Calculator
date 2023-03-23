d797d97edb75c105635c6690422f437cb84e7924
Abel D <70660536+Abel-D@users.noreply.github.com>
2023-03-23 16:07:33 +0300

    added entry main function logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Hours_And_Minutes;
+
+int hr, min = 0;
+TimeModel time;
+Console.WriteLine("Analogue clock hour degree calculator \r");
+Console.WriteLine("_____________________________________ \n");
+Console.WriteLine("Enter hour\n");
+hr = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter minutes\n");
+min = Convert.ToInt32(Console.ReadLine());
+time = new TimeModel(hr, min);
+
+var degrees = DegreeCalculator.CalculateTotalDegrees(time);
+Console.WriteLine(degrees + " degrees");
