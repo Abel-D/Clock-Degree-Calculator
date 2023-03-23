ea64b5b0f43704ba4d507bb1ed9bad54d33eadc6
Abel D <70660536+Abel-D@users.noreply.github.com>
2023-03-23 15:57:25 +0300

    added time model

---
+++ TimeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock_Degree_Calculator
+{
+    internal class TimeModel
+    {
+        public TimeModel(int hour,int minutes)
+        {
+            this.Hour = hour;
+            this.Minutes = minutes; 
+        }
+        public float Hour { get; set; }
+        public float Minutes { get; set; }
+    }
+}
